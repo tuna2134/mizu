d35063697dd23cfed18dfba3bd9c402fd5b5b6c5
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-19 21:57:17 +0900

    Update performance3.py

--- tests/performance3.py
+++ tests/performance3.py
@@ -1,3 +1,17 @@
+from time import time
+
 from mistune import html
 
 
+with open("../README.md", "r") as f:
+    content = f.read()
+    
+
+times = 0
+
+for i in range(10000):
+    first = time()
+    html(content)
+    times += time() - first
+
+print(times)
