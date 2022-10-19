421bbf9c671c3e54cb89c3249326c404bb9381d9
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-19 22:07:39 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod parser;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn pulldown_py(_py: Python, m: &PyModule) -> PyResult<()> {
+fn mizu(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parser::parse, m)?)?;
     Ok(())
 }
