46c0db8b1adc0c4b9cd3258af0fe5853b4f69edd
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-19 22:42:18 +0900

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,12 @@ use pulldown_cmark::{html, Parser};
 
 
 /// This function is parse markdown to html.
+///
+/// Args:
+///   text (str): Markdown content.
+///
+/// Returns:
+///   str: Html content.
 #[pyfunction]
 pub fn parse(text: &str) -> PyResult<String> {
     let parser: Parser = Parser::new(text);
