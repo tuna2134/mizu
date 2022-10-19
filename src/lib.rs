3cf348221c6f3bc4ae92b54c3c6ea9e3e67f9347
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-19 21:08:37 +0900

    Fix: 色々

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 use pyo3::prelude::*;
 mod parser;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pulldown_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(parser::parser, m)?)?;
     Ok(())
 }
