aa8a8aa49fc56b6e541e6361f20375e74b32ef83
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-20 07:45:08 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 mod parser;
 
-/// A Python module implemented in Rust.
+/// Mizu's core
 #[pymodule]
 fn mizu(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parser::parse, m)?)?;
