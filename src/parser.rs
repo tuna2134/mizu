6360cd7811a00b8522ca53501302af70b3ffd447
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-19 21:43:49 +0900

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
-use pulldown_cmark::Parser;
+use pulldown_cmark::{html, Parser};
 
 
 #[pyfunction]
@@ -8,6 +8,6 @@ pub fn parse(text: &str) -> PyResult<String> {
     let parser: Parser = Parser::new(text);
     
     let mut output: String = String::new();
-    pulldown_cmark::html::push_html(&mut output, parser);
+    html::push_html(&mut output, parser);
     Ok(output)
 }
