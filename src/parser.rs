e064f6835b0fe1d527a04fde98d343481f047510
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-20 07:55:58 +0900

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
-use pulldown_cmark::{html, Parser};
+use pulldown_cmark::{html, Options, Parser};
 
 
 /// This function is parse markdown to html.
@@ -15,6 +15,19 @@ pub fn parse(text: &str) -> PyResult<String> {
     let parser: Parser = Parser::new(text);
     
     let mut output: String = String::new();
+    html::push_html(&mut output, parser);
+    Ok(output)
+}
+
+
+#[pyfunction]
+pub fn parse_ext(text: &str, tables: Option<bool>) {
+    let mut options = Options::empty();
+    if tables.unwrap_or(false) {
+        options.insert(Options::ENABLE_TABLES);
+    }
+    let mut output = String::new();
+    Parser::new_ext(text, options);
     html::push_html(&mut output, parser);
     Ok(output)
 }
