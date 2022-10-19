9ef160eb665acff154657d35fe1e4391b8a09924
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-19 21:05:00 +0900

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ use pulldown_cmark::Parser;
 
 #[pyfunction]
 pub fn parser(text: &str) -> PyResult<String> {
-    let parser: Parser = Parser::new();
+    let parser: Parser = Parser::new(text);
     
     let mut output: String = String::new();
     pulldown_cmark::html::push_html(&mut output, parser);
