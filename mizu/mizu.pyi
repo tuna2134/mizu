e8ff8d77c171cf8b7d3bb55a3025177dbf27bd85
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-20 13:05:48 +0900

    add: parse_ext

--- mizu/mizu.pyi
+++ mizu/mizu.pyi
@@ -1,5 +1,13 @@
+from typing import Optional
+
+
 def parse(text: str) -> str:
     ...
 
 
-def parse_ext(text: str, 
+def parse_ext(
+    text: str, tables: Optional[bool], footnotes: Optional[bool],
+    strikethrough: Optional[bool], tasklists: Optional[bool],
+    smart_punctuation: Optional[bool], heading_attribute: Optional[bool]
+) -> str:
+    ...
