c805b3f18c13bcf6e74523cb67cda1cf6b3c21a6
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-29 04:36:28 +0000

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../mizu'))
+
+
 project = 'mizu'
 copyright = '2022, tuna2134'
 author = 'tuna2134'
@@ -13,7 +19,10 @@ author = 'tuna2134'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "furo"
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
