0b3eb9aa7805c4cc0d04ab614a2bbe20d7b7cb6b
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-03 13:30:55 +0900

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -11,6 +11,7 @@ use pulldown_cmark::{html, Options, Parser};
 /// Returns:
 ///     str: Html content.
 #[pyfunction]
+#[args(text)]
 pub fn parse(text: &str) -> PyResult<String> {
     let parser: Parser = Parser::new(text);
     
@@ -34,6 +35,7 @@ pub fn parse(text: &str) -> PyResult<String> {
 /// Returns:
 ///     str: Html content.
 #[pyfunction]
+#[args(text, *, tables, footnotes, strikethrough, tasklists, smart_punctuation, heading_attribute)]
 pub fn parse_ext(
     text: &str, tables: Option<bool>, footnotes: Option<bool>,
     strikethrough: Option<bool>, tasklists: Option<bool>,
