0af536237ad85e4f23145f184feb96c69e09a91e
mc_fdc <Masato.04.11.2007@gmail.com>
2022-12-18 12:35:47 +0900

    delete

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ mod parser;
 /// Mizu's core
 #[pymodule]
 fn mizu(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(parser::parse, m)?)?;
-    m.add_function(wrap_pyfunction!(parser::parse_ext, m)?)?;
     m.add_class::<parser::Markdown>()?;
     Ok(())
 }
